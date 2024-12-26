88d8f0be5081d4840ba13b7e1a4a9c533b6c8bfe
Randy <randyharrogates@gmail.com>
2024-12-26 14:07:08 +0800

    Added files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 /** @format */
 
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css"; // Import Bootstrap Icons
 
 import AboutMe from "./pages/AboutMe.tsx";
-import Project1 from "./pages/Project1";
-import Project2 from "./pages/Project2";
-import Project3 from "./pages/Project3";
-import Project4 from "./pages/Project4";
-import Project5 from "./pages/Project5";
+import Project1 from "./pages/Project1.tsx";
+import Project2 from "./pages/Project2.tsx";
+import Project3 from "./pages/Project3.tsx";
+import Project4 from "./pages/Project4.tsx";
+import Project5 from "./pages/Project5.tsx";
 
 const App: React.FC = () => {
 	return (
@@ -19,7 +20,7 @@ const App: React.FC = () => {
 				<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 					<div className="container-fluid">
 						<Link className="navbar-brand" to="/">
-							My Portfolio
+							<i className="bi-house-door me-2"></i> My Portfolio
 						</Link>
 						<button
 							className="navbar-toggler"
@@ -36,32 +37,32 @@ const App: React.FC = () => {
 							<ul className="navbar-nav">
 								<li className="nav-item">
 									<Link className="nav-link" to="/">
-										About Me
+										<i className="bi-house-door me-2"></i> About Me
 									</Link>
 								</li>
 								<li className="nav-item">
 									<Link className="nav-link" to="/project1">
-										Project 1
+										<i className="bi-gear me-2"></i> Multi-Agent RAG
 									</Link>
 								</li>
 								<li className="nav-item">
 									<Link className="nav-link" to="/project2">
-										Project 2
+										<i className="bi-mic me-2"></i> Speech-To-Text
 									</Link>
 								</li>
 								<li className="nav-item">
 									<Link className="nav-link" to="/project3">
-										Project 3
+										<i className="bi-wrench me-2"></i> Fine-Tuning
 									</Link>
 								</li>
 								<li className="nav-item">
 									<Link className="nav-link" to="/project4">
-										Project 4
+										<i className="bi-code-slash me-2"></i> Web App
 									</Link>
 								</li>
 								<li className="nav-item">
 									<Link className="nav-link" to="/project5">
-										Project 5
+										<i className="bi bi-file-earmark-fill"></i> Web Page
 									</Link>
 								</li>
 							</ul>
@@ -69,14 +70,14 @@ const App: React.FC = () => {
 					</div>
 				</nav>
 				<div className="container mt-4">
-					<Switch>
-						<Route exact path="/" component={AboutMe} />
-						<Route path="/project1" component={Project1} />
-						<Route path="/project2" component={Project2} />
-						<Route path="/project3" component={Project3} />
-						<Route path="/project4" component={Project4} />
-						<Route path="/project5" component={Project5} />
-					</Switch>
+					<Routes>
+						<Route path="/" element={<AboutMe />} />
+						<Route path="/project1" element={<Project1 />} />
+						<Route path="/project2" element={<Project2 />} />
+						<Route path="/project3" element={<Project3 />} />
+						<Route path="/project4" element={<Project4 />} />
+						<Route path="/project5" element={<Project5 />} />
+					</Routes>
 				</div>
 			</div>
 		</Router>
