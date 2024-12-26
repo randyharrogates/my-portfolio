a42527f69af25b52a13d2b1eb4cf9ed93352a1a3
Randy <randyharrogates@gmail.com>
2024-12-26 14:07:08 +0800

    Added files

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-
+import "./index.css"; // Ensure the file exists or remove it if not needed
+import App from "./App.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
@@ -10,5 +9,3 @@ root.render(
 		<App />
 	</React.StrictMode>
 );
-
-
